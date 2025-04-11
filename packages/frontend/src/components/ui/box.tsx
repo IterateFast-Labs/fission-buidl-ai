d7d3fa6088829547c495ecfbd21e570443409be9
mathiaskim522 <hjjam100@gmail.com>
2025-04-11 14:03:07 +0900

    Add Box component with customizable element type and styles

---
+++ packages/frontend/src/components/ui/box.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/lib/tailwind-util';
+
+export interface BoxProps {
+  as?: React.ElementType;
+  className?: string;
+  children?: React.ReactNode;
+  label?: string;
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+export function Box(props: BoxProps) {
+  const Element = props.as || 'div';
+
+  return (
+    <Element
+      ref={props.ref}
+      className={cn([
+        'relative p-4 border-[6px] border-double border-primary/60 bg-material',
+        props.className,
+      ])}
+    >
+      {props.label && (
+        <div className="px-3 w-fit absolute -top-4 left-2 text-base h-6 flex justify-center items-center bg-primary text-white">
+          {props.label}
+        </div>
+      )}
+      {props.children}
+    </Element>
+  );
+}
